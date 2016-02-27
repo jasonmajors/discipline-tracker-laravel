c493ed8d9ace748fa005e56d32f3c701c73c8294
Jason Majors <jasonrmajors@gmail.com>
2016-02-26 21:28:17 -0800

    Created Employee routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,4 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
+// Authentication Routes
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+// Registration Routes
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+// Employee Routes
+Route::get('/employees', 'EmployeeController@index');
+Route::post('/employee', 'EmployeeController@store');
+Route::delete('/employee', 'EmployeeController@destroy');

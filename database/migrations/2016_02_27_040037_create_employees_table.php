15f70e26fff50af35ca76845b6870d7d2cf1fdaf
Jason Majors <jasonrmajors@gmail.com>
2016-02-26 20:12:55 -0800

    Created table migrations for employees and disciplines tables

---
+++ database/migrations/2016_02_27_040037_create_employees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('emp_id');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('dept');
+            $table->string('title');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('employees');
+    }
+}
---
+++ database/migrations/2016_02_27_040752_create_disciplines_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDisciplinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('disciplines', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('employee_id')->index();
+            $table->date('effective');
+            $table->string('issued_by');
+            $table->string('entered_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('disciplines');
+    }
+}

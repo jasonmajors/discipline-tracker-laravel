67ab6cc8c8af31ec6324410b254ec630c652d5f8
Jason Majors <jasonrmajors@gmail.com>
2016-02-26 21:07:32 -0800

    Added Employee and Discipline models

---
+++ app/Discipline.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Discipline extends Model
+{
+    /**
+    * The attributes that are mass-assignable
+    *
+    * @var array
+    */
+    protected $fillable = [	'type',
+    						'effective',
+    						'issued_by',
+    					];
+
+    public function employee()
+    {
+    	return $this->belongsTo(Employee::class);
+    }
+}
---
+++ app/Employee.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    /**
+    * The attributes that are mass-assignable
+    *
+    * @var array
+    */
+    protected $fillable = [	'emp_id', 
+    						'firstname',
+    						'lastname',
+    						'dept',
+    						'title',
+    					];
+    /**
+    * Get all the disciplines for the employee
+    */
+    public function disciplines()
+    {
+    	return $this->hasMany(Discipline::class);
+    }
+}

dead547f6f9f04311d174d5cc87524e626fd520c
Jason Majors <jasonrmajors@gmail.com>
2016-02-27 09:46:49 -0800

    Added table to show current employees to employees/index

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,4 +1,24 @@
 @extends('layouts.app')
 @section('content')
     <h1 class="text-center">Employees</h1>
+    <table class="table">
+        <thead>
+            <th>ID</th>
+            <th>Last Name</th>
+            <th>First Name</th>
+            <th>Department</th>
+            <th>Title</th>
+        </thead>
+        <tbody>
+            @foreach ($employees as $employee)
+                <tr>
+                    <td>{{ $employee->emp_id }}</td>
+                    <td>{{ $employee->lastname }}</td>
+                    <td>{{ $employee->firstname }}</td>
+                    <td>{{ $employee->dept }}</td>
+                    <td>{{ $employee->title }}</td>
+                </tr>
+            @endforeach        
+        </tbody>
+    </table>        
 @endsection

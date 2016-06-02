3d9e5c98e0f44d767c34a1084be3ab4677eab876
Jason Majors <jasonrmajors@gmail.com>
2016-06-01 19:49:53 -0700

    Fix quick issue with displaying the type

--- resources/views/discipline/show.blade.php
+++ resources/views/discipline/show.blade.php
@@ -3,7 +3,7 @@
     <h1 class="text-center">{{ $discipline->employee->firstname}} {{ $discipline->employee->lastname }}</h1>
     <h2 class="text-center">Issued: {{ $discipline->effective->format('m/d/Y')}}</h2>
     <p class="text-left">Issued by: {{ $discipline->issued_by }}</p>
-    <p class="text-left">Type: {{ $discipline->type }}</p>
+    <p class="text-left">Type: {{ $discipline->type->type }}</p>
     <p class="text-left">Reason: {{ $discipline->reason }}</p>
     <p class="text-left">Description: {{ $discipline->description }}</p>
     <a href="{{ action('DisciplineController@edit', ['id' => $discipline->id]) }}">
